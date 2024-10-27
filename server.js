09103d2aff56d533be82313d9d891f722bf4fdf6
anshraiyani <anshraiyani13@gmail.com>
2024-10-27 17:25:15 +0530

    added cors

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const multer = require('multer');
-const { uploadVideo, checkStatus } = require('./controllers/videoController');
+const express = require("express");
+const multer = require("multer");
+const { uploadVideo, checkStatus } = require("./controllers/videoController");
+const cors = require("cors");
 
 const app = express();
 const upload = multer(); // Use multer's memory storage for files
 
+app.use(
+    cors({
+        origin: "*", // Allow all origins or specify your React app's URL
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+        allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+    })
+);
 app.use(express.json());
 
 // Define the route for video upload
-app.post('/upload', upload.single('video'), uploadVideo);
-app.get('/check-status', checkStatus);
+app.post("/upload", upload.single("video"), uploadVideo);
+app.get("/check-status", checkStatus);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
