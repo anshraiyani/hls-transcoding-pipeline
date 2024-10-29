7949b381414c6c2ee3787d30eacc7140db4e51d8
anshraiyani <anshraiyani13@gmail.com>
2024-10-29 19:21:28 +0530

    increased limit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { uploadVideo, checkStatus } = require("./controllers/videoController");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app = express();
 const upload = multer(); // Use multer's memory storage for files
@@ -10,6 +11,8 @@ app.use(cors());
 app.options("*", cors());
 
 app.use(express.json());
+app.use(bodyParser.json({ limit: '100mb' })); // Adjust the limit as needed
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 app.get("/api", (req, res) => {
     return res.status(200).json({
